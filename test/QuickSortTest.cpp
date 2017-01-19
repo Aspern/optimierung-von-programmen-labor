f4c8490d7cabd904c265961ddcbbc00837e88fae
Aspern <795841adriboy>
2017-01-19 09:48:55 +0100

    added docu

--- test/QuickSortTest.cpp
+++ test/QuickSortTest.cpp
@@ -13,36 +13,41 @@ public:
 };
 
 TEST_F(QuickSortTest, sortWithNumber) {
-    std::array<float, 6> a = {47.2, 50, -0.5, 0, 42, 50};
+    std::array<float, 16> a = {47.2, 50, -0.5, 0, 42, 50, -1856, -78, 0.2654, 0, 42.5, 188, 33, 1000, 76.35425, 47.2};
 
     opl::quickSort(a);
 
-    ASSERT_FLOAT_EQ(a[0] , -0.5);
-    ASSERT_FLOAT_EQ(a[1] , 0);
-    ASSERT_FLOAT_EQ(a[2] , 42);
-    ASSERT_FLOAT_EQ(a[3] , 47.2);
-    ASSERT_FLOAT_EQ(a[4] , 50);
-    ASSERT_FLOAT_EQ(a[5] , 50);
-}
-
-TEST_F(QuickSortTest, sortWithBool) {
-    std::array<bool, 2> a = {true, false};
-
-    opl::quickSort(a);
-
-    ASSERT_EQ(a[0] , false);
-    ASSERT_EQ(a[1] , true);
+    ASSERT_FLOAT_EQ(a[0] , -1856);
+    ASSERT_FLOAT_EQ(a[1] , -78);
+    ASSERT_FLOAT_EQ(a[2] , -0.5);
+    ASSERT_FLOAT_EQ(a[3] , 0);
+    ASSERT_FLOAT_EQ(a[4] , 0);
+    ASSERT_FLOAT_EQ(a[5] , 0.2654);
+    ASSERT_FLOAT_EQ(a[6] , 33);
+    ASSERT_FLOAT_EQ(a[7] , 42);
+    ASSERT_FLOAT_EQ(a[8] , 42.5);
+    ASSERT_FLOAT_EQ(a[9] , 47.2);
+    ASSERT_FLOAT_EQ(a[10] , 47.2);
+    ASSERT_FLOAT_EQ(a[11] , 50);
+    ASSERT_FLOAT_EQ(a[12] , 50);
+    ASSERT_FLOAT_EQ(a[13] , 76.35425);
+    ASSERT_FLOAT_EQ(a[14] , 188);
+    ASSERT_FLOAT_EQ(a[15] , 1000);
 }
 
 TEST_F(QuickSortTest, sortWithString) {
-    std::array<std::string, 4> a = {"Z", "G", "K", "A"};
+    std::array<std::string, 8> a = {"D", "B", "H", "A", "E", "F", "H", "C"};
 
     opl::quickSort(a);
 
     ASSERT_EQ(a[0] , "A");
-    ASSERT_EQ(a[1] , "G");
-    ASSERT_EQ(a[2] , "K");
-    ASSERT_EQ(a[3] , "Z");
+    ASSERT_EQ(a[1] , "B");
+    ASSERT_EQ(a[2] , "C");
+    ASSERT_EQ(a[3] , "D");
+    ASSERT_EQ(a[4] , "E");
+    ASSERT_EQ(a[5] , "F");
+    ASSERT_EQ(a[6] , "H");
+    ASSERT_EQ(a[7] , "H");
 }
 
 TEST_F(QuickSortTest, sortHybridWithNumber) {
@@ -78,12 +83,16 @@ TEST_F(QuickSortTest, sortHybridWithBool) {
 }
 
 TEST_F(QuickSortTest, sortHybridWithString) {
-    std::array<std::string, 4> a = {"Z", "G", "K", "A"};
+    std::array<std::string, 8> a = {"D", "B", "H", "A", "E", "F", "H", "C"};
 
     opl::quickSortHybrid(a);
 
     ASSERT_EQ(a[0] , "A");
-    ASSERT_EQ(a[1] , "G");
-    ASSERT_EQ(a[2] , "K");
-    ASSERT_EQ(a[3] , "Z");
+    ASSERT_EQ(a[1] , "B");
+    ASSERT_EQ(a[2] , "C");
+    ASSERT_EQ(a[3] , "D");
+    ASSERT_EQ(a[4] , "E");
+    ASSERT_EQ(a[5] , "F");
+    ASSERT_EQ(a[6] , "H");
+    ASSERT_EQ(a[7] , "H");
 }
